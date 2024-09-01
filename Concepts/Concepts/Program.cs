0b812ff001d0763b7f0b340497fceb732f49af0e
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-09-01 14:28:39 -0300

    using OrderBy

--- Concepts/Concepts/Program.cs
+++ Concepts/Concepts/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Balta.ContentContext;
 
 namespace MeuPrograma
@@ -33,16 +34,19 @@ namespace MeuPrograma
 
             var careers = new List<Career>();
             var careerDotnet = new Career("Especialista .NET", "especialista-dotnet");
+            var careerItem2 = new CareerItem(3, "Aprenda .Net", "", null);
             var careerItem = new CareerItem(1, "Comece por aqui", "", null);
-            var careerItem2 = new CareerItem(2, "Aprenda .Net", "", null);
+            var careerItem3 = new CareerItem(2, "Aprenda OOP", "", null);
             
             careerDotnet.Items.Add(careerItem2);
             careerDotnet.Items.Add(careerItem);
+            careerDotnet.Items.Add(careerItem3);
             careers.Add(careerDotnet);
             
             foreach(var carrer in careers){
                 Console.WriteLine(carrer.Title);
-                foreach(var item in carrer.Items){
+                // Usaremos o Order para ordenação de listas
+                foreach(var item in carrer.Items.OrderBy(x => x.Order)){
                     Console.WriteLine($"{item.Order}-{item.Title}");
                 }
             }
